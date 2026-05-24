93f356136bf44ee42afd100c95315bce71a81bb3
bharathkumarp7736 <bharathkumarp7736@gmail.com>
2026-05-24 13:20:57 +0530

    stripe payment fix

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectCloudinary()
 
 
 //allow multiple origins
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['http://localhost:5173','https://greencart-3vpu.onrender.com']
 
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebhook)
 
